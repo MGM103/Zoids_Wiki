e9c23356402d6ca735f7992b791ca0a41a0d0984
Marcus Marinelli <marcus.marinelli3@gmail.com>
2022-06-23 22:48:45 +1000

    Created Row component with custom type

--- zoids_app/src/App.js
+++ zoids_app/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import './App.css';
 
 import zoids from "./zoids.json";
+
+const WikiRow = ({zoids}) => (
+  <tr>
+    <td>{zoids.name}</td>
+    <td>{zoids.type}</td>
+    <td>{zoids.height}</td>
+    <td>{zoids.length}</td>
+    <td>{zoids.weight}</td>
+    <td>{zoids.max_speed}</td>
+    <td>{zoids.weapons.join(", ")}</td>
+    <td>{zoids.equipment.join(", ")}</td>
+  </tr>
+);
+
+WikiRow.propTypes = {
+  zoids: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    height: PropTypes.string,
+    length: PropTypes.string,
+    weight: PropTypes.string,
+    max_speed: PropTypes.string,
+    weapons: PropTypes.arrayOf(PropTypes.string),
+    equipment: PropTypes.arrayOf(PropTypes.string)
+  })
+};
 
 function App() {
   return (
@@ -26,16 +53,7 @@ function App() {
           </thead>
           <tbody>
             {zoids.map((zoids) => (
-              <tr>
-                <td>{zoids.name}</td>
-                <td>{zoids.type}</td>
-                <td>{zoids.height}</td>
-                <td>{zoids.length}</td>
-                <td>{zoids.weight}</td>
-                <td>{zoids.max_speed}</td>
-                <td>{zoids.weapons.join(", ")}</td>
-                <td>{zoids.equipment.join(", ")}</td>
-              </tr>
+              <WikiRow zoids={zoids} key={zoids.id} />
             ))}
           </tbody>
         </table>
